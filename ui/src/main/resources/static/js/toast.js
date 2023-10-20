6e53278f490ba7930d9ed228a05e33825c3ded5f
Gaurav Patel <gaurav.patel@incedoinc.com>
2023-10-20 11:36:34 -0400

    Gaurav

---
+++ ui/src/main/resources/static/js/toast.js
@@ -0,0 +1,6 @@
+function showToast(message) {
+    const toastElement = document.getElementById('notificationToast');
+    toastElement.querySelector('.toast-body').textContent = message;
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+}
