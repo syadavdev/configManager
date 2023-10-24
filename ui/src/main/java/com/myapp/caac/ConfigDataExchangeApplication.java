a0b06fd9274bd218cdb662ff9cbee63422b83118
Gaurav Patel <gaurav.patel@incedoinc.com>
2023-10-24 10:02:03 -0400

    123

--- ui/src/main/java/com/myapp/caac/ConfigDataExchangeApplication.java
+++ ui/src/main/java/com/myapp/caac/ConfigDataExchangeApplication.java
@@ -1,7 +1,5 @@
 package com.myapp.caac;
 
-import com.myapp.caac.fetcher.PropertiesFetcher;
-import com.myapp.caac.fetcher.PropertiesFileFetcher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +11,7 @@ import java.util.Properties;
 
 @SpringBootApplication
 @Slf4j
-public class ConfigDataExchangeApplication implements CommandLineRunner {
+public class ConfigDataExchangeApplication {
 
 
     private final ResourceLoader resourceLoader;
@@ -26,27 +24,4 @@ public class ConfigDataExchangeApplication implements CommandLineRunner {
         SpringApplication.run(ConfigDataExchangeApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("ConfigDataExchangeApplication started successfully");
-
-//        String classpathResource = "classpath:application.properties";
-//        String filePath = "file:/Users/gaurav/Library/Mobile Documents/com~apple~CloudDocs/Documents/Code/projects/java/ConfigDataExchange/src/main/resources/my-application.properties";
-//
-//        // Fetch properties from different sources
-//        Map<String, Properties> propertiesMap = new HashMap<>();
-//        propertiesMap.put(classpathResource, fetchProperties(classpathResource));
-//        propertiesMap.put(filePath, fetchProperties(filePath));
-//
-//        // Inside your run method or wherever you are processing the properties
-//        PropertiesToJsonConverter converter = new PropertiesToJsonConverterImpl();
-//        String json = converter.convertAllToJson(propertiesMap);
-//        System.out.println(json);
-    }
-
-
-    private Properties fetchProperties(String resourceLocation) throws IOException {
-        PropertiesFetcher propertiesFetcher = new PropertiesFileFetcher(resourceLocation, resourceLoader);
-        return propertiesFetcher.fetchProperties();
-    }
 }
